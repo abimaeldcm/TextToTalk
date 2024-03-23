4578b7dff169c621fde8a3bcbb16c7621d13d538
abima <abima@DESKTOP-FKO1IBA>
2024-03-23 10:46:01 -0300

    Add project files.

---
+++ TextToTalk/Controllers/TextToSpeechController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TextToTalk.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TextToSpeechController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string text)
+        {
+            try
+            {
+                var audioData = await TextToSpeechService.TextToVoiceService(text);
+                return File(audioData, "audio/wav");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ TextToTalk/Service/TextToSpeechService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CognitiveServices.Speech;
+
+public static class TextToSpeechService
+{
+    // Este exemplo requer variáveis de ambiente chamadas "SPEECH_KEY" e "SPEECH_REGION"
+    static string speechKey = "CHAVE";
+    static string speechRegion = "eastus";
+
+    public static async Task<byte[]> TextToVoiceService(string text)
+    {
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+
+        // O idioma da voz que fala.
+        speechConfig.SpeechSynthesisVoiceName = "pt-BR-BrendaNeural";
+
+        // use the default speaker as audio output.
+        using (var synthesizer = new SpeechSynthesizer(speechConfig))
+        {
+            var result = await synthesizer.SpeakTextAsync(text);
+
+            if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+            {
+                return result.AudioData;
+            }
+            else
+            {
+                throw new Exception($"Falha ao sintetizar áudio: {result.Reason}");
+            }
+        }        
+    }
+}
